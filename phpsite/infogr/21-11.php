3f19df3cb37f63d80e5d3fb9ad308c136fafb474
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2023-11-21 22:54:36 +0200

    Christian Family Day

--- phpsite/infogr/21-11.php
+++ phpsite/infogr/21-11.php
@@ -1,21 +1,22 @@
 <?php if ($lang == 'bg') : ?>
   На този ден православната църква чества 
   <a href="https://bg.wikipedia.org/wiki/%D0%92%D1%8A%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5_%D0%91%D0%BE%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%B8%D1%87%D0%BD%D0%BE">Въведение на Пресвета Богородица в храма</a>
- (<i>Въведение Богородично</i>).
+ (<i>Въведение Богородично</i>; <i>Ден на християнското семейство</i>).
 
 <?php elseif ($lang == 'en') : ?>
   On this day the Ortodox church celebrates 
   <a href="https://en.wikipedia.org/wiki/Presentation_of_Mary">The Entry of the Most Holy Theotokos into the Temple</a>
- (<i>Presentation of Mary</i>).
+ (<i>Presentation of Mary</i>; <i>Christian Family Day</i>).
   
 <?php elseif ($lang == 'de') : ?>
   An diesem Tag feiert die ortodoxe Kirche
   <a href="https://de.wikipedia.org/wiki/Gedenktag_Unserer_Lieben_Frau_in_Jerusalem">Gedenktag Unserer Lieben Frau in Jerusalem</a>
- (<i>Darstellung Mariens im Tempel</i>).
+ (<i>Darstellung Mariens im Tempel</i>;<i>Christlicher Familientag</i>).
 
 <?php elseif ($lang == 'ru') : ?>
   В этот день православная церковь празднует
   <a href="https://ru.wikipedia.org/wiki/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2%D0%BE_%D1%85%D1%80%D0%B0%D0%BC_%D0%9F%D1%80%D0%B5%D1%81%D0%B2%D1%8F%D1%82%D0%BE%D0%B9_%D0%91%D0%BE%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%B8%D1%86%D1%8B">Введение во храм Пресвятой Богородицы</a>
+  (<i>День христианской семьи</i>)
 
 <?php endif ?>
 
